f8fe89b4d7ba9c216d37c844096399799c94d3dd
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-12 08:17:33 +0530

    Update script.js added the dom statments for clock time and audio file and alaram lsit

---
+++ script.js
@@ -0,0 +1,5 @@
+let clock = document.getElementById("clock")
+let alarm_time = document.getElementById("alarm_time")
+let audio_file = document.getElementById("audio_file")
+let alarm_list = document.getElementById("alarm_list")
+let alarms = []
