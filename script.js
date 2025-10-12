63d0342f39ab2cb1a327b8fa392fe664c071ce05
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-12 08:18:13 +0530

    ADDED THE SHOWTIME FUNCTIONS TO SHOW THE TIME IN INDIA STANDARTDS

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ let alarm_time = document.getElementById("alarm_time")
 let audio_file = document.getElementById("audio_file")
 let alarm_list = document.getElementById("alarm_list")
 let alarms = []
+
+function show_time() {
+  let now = new Date()
+  let time_str = now.toLocaleTimeString('en-US', { hour12: true })
+  clock.textContent = time_str
+  alarms.forEach(a => {
+    if (!a.done && time_str === a.time) {
+      let s = new Audio(a.sound)
+      s.play()
+      alert("⏰ Alarm for " + a.time + " is ringing!")
+      a.done = true
+    }
+  })
+}
+
